2dff8f0073eb78630cf6c10430fda34c9b4b36ce
karabenemsi <florian@florianlubitz.de>
2018-12-09 19:40:34 +0100

    Build

--- js/main.js
+++ js/main.js
@@ -1,29 +1,26 @@
 
 const ScrollMagic = require('scrollmagic');
-require('../node_modules/scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators');
 
 let controller = new ScrollMagic.Controller();
 
-
 let scene = new ScrollMagic.Scene({
-  triggerElement: '#trigger-1',
+  triggerElement: '#js-trigger-header',
   triggerHook: 'onLeave',
-  offset: 10
-}).setClassToggle('#element-1','new-class')
-.addIndicators()
+  offset: 500
+}).setClassToggle('#js-header', 'topbar')
 .addTo(controller);
 
 
 
-let durationScene = new ScrollMagic.Scene({
-  triggerElement: '#trigger-2',
-    triggerHook: 'onLeave',
-  duration: 1000
-}).on("enter", () => {
-  console.log('Do Something on Enter');
-  // Do something
-}).on('leave', () => {
-  // Do something
-  console.log('Do Something on leave');
-}).addIndicators()
-.addTo(controller);
+// let durationScene = new ScrollMagic.Scene({
+//   triggerElement: '#trigger-2',
+//     triggerHook: 'onLeave',
+//   duration: 1000
+// }).on("enter", () => {
+//   console.log('Do Something on Enter');
+//   // Do something
+// }).on('leave', () => {
+//   // Do something
+//   console.log('Do Something on leave');
+// }).addIndicators()
+// .addTo(controller);
